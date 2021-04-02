6aa4cf952d00083b5e27e61fbbb1901ddaa531e2
Bryan J. Brown <bjbrown@fsu.edu>
2021-04-02 21:51:15 +0000

    Updates to logging

--- ais.py
+++ ais.py
@@ -107,6 +107,11 @@ def set_diginole_ais_log_status(value):
   drush_vset.append(drushcmd)
   output = subprocess.check_output(drush_vset)
 
+def check_if_iid_exists_elsewhere(iid):
+  drushcmd = 'docker exec {0} bash -c "drush  --root=/var/www/html php-eval \\"module_load_include(\'inc\', \'diginole_purlz\', \'includes/utilities\'); echo json_encode(diginole_purlz_search_iid(\'{1}\'));\\""'.format(apache_name, iid)
+  output = json.loads(os.popen(drushcmd).read())
+  return output
+
 def create_preprocess_package(package_name):
   os.system("zip -d {0}/{1} manifest.ini {2}".format(package_path, package_name, silence_output))
   os.system("mv {0}/{1} {0}/{1}.preprocess".format(package_path, package_name))
@@ -137,11 +142,11 @@ def list_new_packages():
       if package_age > s3_wait:
         package_extension = get_file_extension(package_name) 
         if package_extension != 'zip':
-          log("New package {0}/new/{1} detected, but is not a zip archive.".format(s3_path, package_name), log_file = package_name)
+          log("Failed to validate with the following errors: {0} is not a zip archive.".format(package_name), log_file = package_name)
           move_s3_file("s3://{0}/new/{1}".format(s3_path, package_name), "s3://{0}/error/{1}".format(s3_path, package_name))
           move_s3_file("{0}/{1}.log".format(package_path, package_name), "s3://{0}/error/{1}.log".format(s3_path, package_name))
           log("Package and log data moved to s3://{0}/error/.".format(s3_path), log_file = False)
-          write_to_drupal_log(current_time, current_time, package_name, 'Invalid', "{0} is not a zip archive; AIS packages must be zip archives.".format(package_name))
+          write_to_drupal_log(current_time, current_time, package_name, 'Invalid', "Failed to validate with the following errors: {0} is not a zip archive.".format(package_name))
         else:
           package_key = str(package_mod_timestamp)
           packages[package_key] = package_name
@@ -173,73 +178,83 @@ def validate_package(package_name):
   os.system("zip -d {0}/{1} __MACOSX/\* {2}".format(package_path, package_name, silence_output))
   package = zipfile.ZipFile("{0}/{1}".format(package_path, package_name), 'r')
   package_contents = package.namelist()
-  if 'manifest.ini' not in package_contents:
-    package_errors.append('Missing manifest.ini file')
+  subfolder_files = []
+  for filename in package_contents:
+    splitfilename = filename.split('/')
+    if len(splitfilename) > 1:
+      if splitfilename[1]:
+        subfolder_files.append(filename)
+  if len(subfolder_files) > 0:
+    joined_subfolder_files = ', '.join(subfolder_files)
+    package_errors.append("package contains files in subdirectories: [{0}]".format(joined_subfolder_files))
   else:
-    manifest = configparser.ConfigParser()
-    manifest.read_string(package.read('manifest.ini').decode('utf-8'))
-    if 'package' not in manifest.sections():
-      package_errors.append('manifest.ini missing [package] section')
+    if 'manifest.ini' not in package_contents:
+      package_metadata['submitter_email'] = False
+      package_metadata['content_model'] = False
+      package_metadata['parent_collection'] = False
+      package_errors.append('missing manifest.ini file')
     else:
-      if 'submitter_email' in manifest['package'].keys():
-        package_metadata['submitter_email'] = manifest['package']['submitter_email'] 
-      else: 
-        package_errors.append('manifest.ini missing submitter_email')
-      if 'content_model' in manifest['package'].keys():
-        package_metadata['content_model'] = manifest['package']['content_model']
-        if package_metadata['content_model'] not in cmodels.keys():
-          package_errors.append("manifest.ini content_model {0} is an invalid content model".format(package_metadata['content_model']))
+      manifest = configparser.ConfigParser()
+      manifest.read_string(package.read('manifest.ini').decode('utf-8'))
+      if 'package' not in manifest.sections():
+        package_errors.append('manifest.ini missing [package] section')
+      else:
+        if 'submitter_email' in manifest['package'].keys():
+          package_metadata['submitter_email'] = manifest['package']['submitter_email'] 
+        else: 
+          package_metadata['submitter_email'] = False
+          package_errors.append('manifest.ini missing submitter_email')
+        if 'content_model' in manifest['package'].keys():
+          package_metadata['content_model'] = manifest['package']['content_model']
+          if package_metadata['content_model'] not in cmodels.keys():
+            package_errors.append("manifest.ini content_model {0} is an invalid content model".format(package_metadata['content_model']))
+            package_metadata['content_model'] = False
+        else: 
           package_metadata['content_model'] = False
-      else: 
-        package_errors.append('manifest.ini missing content_model')
-      if 'parent_collection' not in manifest['package'].keys(): 
-        package_errors.append('manifest.ini missing parent_collection')
-      else: 
-        package_metadata['parent_collection'] = manifest['package']['parent_collection']
-        drushcmd = 'drush --root=/var/www/html/ php-eval "echo islandora_object_load(\'{0}\')->id;"'.format(package_metadata['parent_collection'])
-        drush_parent_check = drush_exec.copy()
-        drush_parent_check.append(drushcmd)
-        output = subprocess.check_output(drush_parent_check)
-        output = output.decode('utf-8').split('\n')
-        if output[0] != package_metadata['parent_collection']:
-          package_errors.append("manifest.ini parent_collection {0} does not exist".format(package_metadata['parent_collection']))
-    package_contents.remove('manifest.ini')
-    if 'content_model' not in package_metadata.keys(): 
-      package_errors.append("cannot continue with validation due to missing content_model in manifest.ini")
-    else:
-      subfolder_files = []
-      for filename in package_contents:
-        splitfilename = filename.split('/')
-        if len(splitfilename) > 1:
-          if splitfilename[1]:
-            subfolder_files.append(filename)
-        elif get_file_extension(filename) == 'xml':
-          try:
-            xmldata = xml.etree.ElementTree.fromstring(package.read(filename).decode('utf-8')) 
-            iid = False
-            identifiers = xmldata.findall('{http://www.loc.gov/mods/v3}identifier')
-            for identifier in identifiers:
-              if identifier.attrib['type'].lower() == 'iid':
-                iid = identifier.text
-                if iid != get_file_basename(filename):
-                  package_errors.append("{0} filename does not match contained IID {1}".format(filename, iid))
-            iid_exempt_cmodels = get_iid_exempt_cmodels()
-            if not iid and package_metadata['content_model'] not in iid_exempt_cmodels:
-              package_errors.append("{0} does not contain an IID".format(filename))
-          except:
-            package_errors.append("Error while attempting to parse {0} (see s3://{1}/error/{2}.log for full error output)".format(filename, s3_path, package_metadata['filename']))
-            exception_error = {'filename': filename, 'exception': sys.exc_info()}
-        else:
-          if package_metadata['content_model'] and package_metadata['content_model'] != 'islandora:binaryObjectCModel' and get_file_extension(filename) not in cmodels[package_metadata['content_model']]:
-            package_errors.append("{0} does not have an approved file extension for {1} objects".format(filename, package_metadata['content_model']))
-          associated_mods = "{0}.xml".format(get_file_basename(filename))
-          if associated_mods not in package_contents:
-            package_errors.append("{0} has no associated MODS record".format(filename))
-      if len(subfolder_files) > 0:
-        joined_subfolder_files = ', '.join(subfolder_files)
-        package_errors.append("package contains files in subdirectories: [{0}]".format(joined_subfolder_files))
+          package_errors.append('manifest.ini missing content_model')
+        if 'parent_collection' not in manifest['package'].keys(): 
+          package_metadata['parent_collection'] = False
+          package_errors.append('manifest.ini missing parent_collection')
+        else: 
+          package_metadata['parent_collection'] = manifest['package']['parent_collection']
+          drushcmd = 'drush --root=/var/www/html/ php-eval "echo islandora_object_load(\'{0}\')->id;"'.format(package_metadata['parent_collection'])
+          drush_parent_check = drush_exec.copy()
+          drush_parent_check.append(drushcmd)
+          output = subprocess.check_output(drush_parent_check)
+          output = output.decode('utf-8').split('\n')
+          if output[0] != package_metadata['parent_collection']:
+            package_errors.append("manifest.ini parent_collection {0} does not exist".format(package_metadata['parent_collection']))
+      package_contents.remove('manifest.ini')
+    for filename in package_contents:
+      if get_file_extension(filename) == 'xml':
+        try:
+          xmldata = xml.etree.ElementTree.fromstring(package.read(filename).decode('utf-8')) 
+          iid = False
+          identifiers = xmldata.findall('{http://www.loc.gov/mods/v3}identifier')
+          for identifier in identifiers:
+            if identifier.attrib['type'].lower() == 'iid':
+              iid = identifier.text
+              if iid != get_file_basename(filename):
+                package_errors.append("{0} filename does not match contained IID {1}".format(filename, iid))
+              else:
+                pids = check_if_iid_exists_elsewhere(iid)
+                if len(pids) > 0:
+                  pidstring = ", ".join(pids)
+                  package_errors.append("{0} filename contains IID {1} already in use by {2}".format(filename, iid, pidstring))
+          iid_exempt_cmodels = get_iid_exempt_cmodels()
+          if not iid and package_metadata['content_model'] and package_metadata['content_model'] not in iid_exempt_cmodels:
+            package_errors.append("{0} does not contain an IID".format(filename))
+        except:
+          package_errors.append("Error while attempting to parse {0} (see s3://{1}/error/{2}.log for full error output)".format(filename, s3_path, package_metadata['filename']))
+          exception_error = {'filename': filename, 'exception': sys.exc_info()}
+      else:
+        if package_metadata['content_model'] and package_metadata['content_model'] != 'islandora:binaryObjectCModel' and get_file_extension(filename) not in cmodels[package_metadata['content_model']]:
+          package_errors.append("{0} does not have an approved file extension for {1} objects".format(filename, package_metadata['content_model']))
+        associated_mods = "{0}.xml".format(get_file_basename(filename))
+        if associated_mods not in package_contents:
+          package_errors.append("{0} has no associated MODS record".format(filename))
   if len(package_errors) > 0:
-    invalid_logmsg = "Package {0} failed to validate with the following errors: {1}.".format(package_name, ', '.join(package_errors))
+    invalid_logmsg = "Failed to validate with the following errors: {0}.".format(', '.join(package_errors))
     log(invalid_logmsg, log_file = package_name)
     if exception_error:
       log("Error while attempting to parse {0}: {1}.".format(exception_error['filename'], exception_error['exception']), log_file = package_name)
@@ -253,7 +268,7 @@ def validate_package(package_name):
     return False
   else:
     package_metadata['status'] = 'validated'
-    log("Package {0} passed validation check.".format(package_name), log_file = package_name)
+    log("Passed validation check.".format(package_name), log_file = package_name)
     return package_metadata
 
 def package_preprocess(package_metadata):
@@ -270,10 +285,11 @@ def package_preprocess(package_metadata):
     output = output.decode('utf-8').strip().split()
     package_metadata['status'] = 'preprocessed'
     package_metadata['batch_set_id'] = output[1]
-    log("{0} preprocessed, assigned Batch Set ID {1}".format(package_metadata['filename'], package_metadata['batch_set_id']), log_file =  package_metadata['filename'])
-  except:
+    log("Preprocessed, assigned Batch Set ID {0}".format(package_metadata['batch_set_id']), log_file =  package_metadata['filename'])
+  except subprocess.CalledProcessError as e:
     package_metadata['status'] = 'failed'
-    log("Exception caught during preprocessing, {0}.preprocess failed with {1}".format(package_metadata['filename'], sys.exc_info()), log_file = package_metadata['filename'])
+    log("Exception caught during preprocessing, failed with {0}".format(sys.exc_info()), log_file = package_metadata['filename'])
+    log(e.output.decode('utf-8'), log_file = package_metadata['filename'])
     move_s3_file("s3://{0}/new/{1}".format(s3_path, package_metadata['filename']), "s3://{0}/error/{1}".format(s3_path, package_metadata['filename']))
     move_s3_file("{0}/{1}.preprocess".format(package_path, package_metadata['filename']), "s3://{0}/error/{1}.preprocess".format(s3_path, package_metadata['filename']))
     move_s3_file("{0}/{1}.log".format(package_path, package_metadata['filename']), "s3://{0}/error/{1}.log".format(s3_path, package_metadata['filename']))
@@ -296,7 +312,7 @@ def package_ingest(package_metadata):
       if output[0].startswith('WD islandora: Failed to ingest object:'):
         package_metadata['status'] = 'failed'
         logstring = "\n".join(output)
-        log("{0} failed to ingest with the following errors:\n{1}".format(package_metadata['filename'], logstring), log_file = package_metadata['filename'])
+        log("Failed to ingest with the following errors:\n{0}".format(logstring), log_file = package_metadata['filename'])
         move_s3_file("s3://{0}/new/{1}".format(s3_path, package_metadata['filename']), "s3://{0}/error/{1}".format(s3_path, package_metadata['filename']))
         move_s3_file("{0}/{1}.preprocess".format(package_path, package_metadata['filename']), "s3://{0}/error/{1}.preprocess".format(s3_path, package_metadata['filename']))
         move_s3_file("{0}/{1}.log".format(package_path, package_metadata['filename']), "s3://{0}/error/{1}.log".format(s3_path, package_metadata['filename']))
@@ -315,8 +331,8 @@ def package_ingest(package_metadata):
         pidstring = ", ".join(pids)
         logstring = "\n".join(loginfo)
         package_metadata['status'] = 'ingested'
-        log("{0} ingested, produced PIDs: {1}".format(package_metadata['filename'], pidstring), log_file = package_metadata['filename'])
-        log("{0} ingestion produced the following log output:\n{1}".format(package_metadata['filename'], logstring), log_file = package_metadata['filename'])
+        log("Ingested, produced PIDs: {0}".format(pidstring), log_file = package_metadata['filename'])
+        log("Ingestion produced the following log output:\n{0}".format(logstring), log_file = package_metadata['filename'])
         move_s3_file("s3://{0}/new/{1}".format(s3_path, package_metadata['filename']), "s3://{0}/done/{1}".format(s3_path, package_metadata['filename']))
         move_s3_file("{0}/{1}.preprocess".format(package_path, package_metadata['filename']), "s3://{0}/done/{1}.preprocess".format(s3_path, package_metadata['filename']))
         move_s3_file("{0}/{1}.log".format(package_path, package_metadata['filename']), "s3://{0}/done/{1}.log".format(s3_path, package_metadata['filename']))
@@ -324,9 +340,10 @@ def package_ingest(package_metadata):
         package_metadata['stop_time'] = get_current_time()
         write_to_drupal_log(package_metadata['start_time'], package_metadata['stop_time'], package_metadata['filename'], 'Success', pidstring)
         set_diginole_ais_log_status("Inactive")
-    except:
+    except subprocess.CalledProcessError as e:
       package_metadata['status'] = 'failed'
       log("Exception caught during ingestion, Batch Set {0} failed with {1}".format(package_metadata['batch_set_id'], sys.exc_info()), log_file = package_metadata['filename'])
+      log(e.output.decode('utf-8'), log_file = package_metadata['filename'])
       move_s3_file("s3://{0}/new/{1}".format(s3_path, package_metadata['filename']), "s3://{0}/error/{1}".format(s3_path, package_metadata['filename']))
       move_s3_file("{0}/{1}.preprocess".format(package_path, package_metadata['filename']), "s3://{0}/error/{1}.preprocess".format(s3_path, package_metadata['filename']))
       move_s3_file("{0}/{1}.log".format(package_path, package_metadata['filename']), "s3://{0}/error/{1}.log".format(s3_path, package_metadata['filename']))
